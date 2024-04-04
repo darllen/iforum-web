3ba2488e50bed2aecc54d71de779a80c143916b9
Anunciada <jamilly.d.castro@accenture.com>
2024-04-04 12:34:54 -0300

    estrutura para tela pergunta

---
+++ src/views/pergunta/Pergunta.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+export default function Pergunta() {
+
+    return (
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
